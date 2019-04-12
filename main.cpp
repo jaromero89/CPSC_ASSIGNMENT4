ddf90c88301ad3d1f4a3c7eb9025f63344bf65f7
jaromero89 <47406325+jaromero89@users.noreply.github.com>
2019-04-11 22:47:22 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 
 #include <iostream>
 #include <fstream>
-#include "Student.h"
+#include <ListNode.h>
+#include "GenDoublyLinkedList.h"
 #include "GenQueue.h"
 using namespace std;
 
+!(Not complete)!
+
+string filename;
+
 int main(int argc, char const *argv[]) {
+
+  cout << "Please enter file: ";
+  getline(cin, filename);
   
   return 0;
 }
